606f92e7b69b580314914c029b28518a054050d5
unknown <szhu@UTILE-T1700-11.Utile.local>
2015-05-07 15:49:43 -0400

    update mode

--- 00_mode/script/script.js
+++ 00_mode/script/script.js
@@ -12,14 +12,17 @@ for(var mIndex = 0; mIndex < 7; mIndex++){
     });
 }
 
+var apiKey = "AIzaSyDwLG95MmzKvGNjhIV5UBd1LjW1JTWjdqk";
+
 //Global visual variables
-var margin = {t:120,r:150,b:80,l:200};
+var margin = {t:160,r:150,b:80,l:200};
 
 var yPadding = 5,
     scaleY = d3.scale.linear(),
     scaleColor = d3.scale.ordinal().domain(d3.range(7)).range(['#d3d1e9','#9abcc3','#1c9ad6','#00b26b','#2fa148','#9ed089','#ef4723']);
     scaleColorByLq = d3.scale.linear().domain([0,1,3]).range(['#03afeb','white','red']);
-    scaleThreshold = d3.scale.threshold().domain([.5,.75,.95,1.05,1.25,1.5]).range(['signficantly below','below','slightly below','similar to','slightly above','above','significantly above']);
+    scaleThreshold = d3.scale.threshold().domain([.5,.75,.95,1.05,1.25,1.5]).range(['signficantly below','below','slightly below','similar to','slightly above','above','significantly above']),
+    scaleX = d3.scale.linear(); //x-scale, 100% adds up to 60% of chart width
 var fisheye = d3.fisheye.circular()
     .radius(60);
 
@@ -35,9 +38,9 @@ var path = d3.geo.path()
 var dispatch = d3.dispatch('chartHover','mapHover','out');
 
 //On window load...
-/*$(window).on('load',function(e){
+$(window).on('load',function(e){
     $('#intro').modal();
-});*/
+});
 
 
 //Load data
@@ -73,7 +76,8 @@ function drawModeChart(mode,ctx){
     //visual variables specific to this viz
     var width = $('#chart').width() - margin.r - margin.l,
         height = $('#chart').height() - margin.t - margin.b;
-    var scaleX = d3.scale.linear().domain([0,1]).range([0,width*.8]); //x-scale, 100% adds up to 60% of chart width
+    scaleX.domain([0,1]).range([0,width*.7]);
+
     var canvas = ctx
         .append('svg')
         .attr('width',width+margin.r+margin.l)
@@ -88,6 +92,53 @@ function drawModeChart(mode,ctx){
         .style('stroke-width','1.5px')
         .style('stroke','white')
         .style('opacity',0)
+    var xTicks = canvas.selectAll('.x-tick')
+        .data([-.5,-.25,0,.25,.5,.75])
+        .enter()
+        .append('g')
+        .attr('class','x-tick')
+        .attr('transform',function(d){
+            return 'translate('+ ( width/2+scaleX(d) ) + ',0)';
+        });
+    xTicks.append('text')
+        .text(function(d){
+            return format(Math.abs(d));
+        })
+        .attr('text-anchor','middle')
+        .attr('dy',-3)
+    xTicks.append('line')
+        .attr('y1',0)
+        .attr('y2',height);
+
+    //Mode legend
+    var modeLegends = canvas.append('g')
+        .attr('class','mode-legend')
+        .attr('transform','translate('+width/2+','+(-60)+')')
+        .selectAll('.bar')
+        .data(mode[0].modeShares)
+        .enter()
+        .append('g')
+        .attr('class','bar')
+        .attr('transform',function(d,i){
+            return 'translate('+ (-width*2/8 + i*width/8) +',0)';
+        });
+    modeLegends
+        .append('rect')
+        .attr('height',4)
+        .attr('width',width/8-2)
+        .style('fill',function(d){
+            return scaleColor(d.mIndex);
+        });
+    modeLegends
+        .append('text')
+        .attr('text-anchor','middle')
+        .text(function(d){return d.mode})
+        .attr('x',width/20)
+        .attr('y',20)
+        .style('fill',function(d){
+            return scaleColor(d.mIndex);
+        })
+
 
     //Nest tracts by neighborhoods
     //And remove those in HI
@@ -154,23 +205,15 @@ function drawModeChart(mode,ctx){
             return scaleColor(d.mIndex);
         });
 
-    canvas.on('mousemove',function(){
-        fisheye.focus(d3.mouse(this));
-
-        node.each(function(d){
-            d.fisheye = fisheye(d);
-            })
-            .attr('transform',function(d){
-                return 'translate('+d.fisheye.x+','+d.fisheye.y+')';
-            });
-
-        shareNode
-            .attr('width',function(d){return d.dx * d.fisheye.z;})
-            .attr('height',function(d){return d.dy * d.fisheye.z;})
-    })
-
     function onShareEnter(s){
         dispatch.chartHover(s); //globally broadcast the id of the tract
+
+        //Color the mode legends
+        modeLegends
+            .style('opacity',.3)
+        modeLegends
+            .filter(function(d){return d.mIndex == s.mIndex})
+            .style('opacity',1);
 
         //position visual target
         visualTarget
@@ -306,266 +349,6 @@ function drawModeChart(mode,ctx){
 
 }
 
-/*function drawTimeChart(tracts,ctx){
-    //visual variables specific to this viz
-    var width = $('#chart').width() - margin.r - margin.l,
-        height = $('#chart').height() - margin.t - margin.b;
-    var cellWidth = width/8;
-
-    var canvas = ctx
-        .append('svg')
-        .attr('width',width+margin.r+margin.l)
-        .attr('height',height + margin.t + margin.b)
-        .append('g')
-        .attr('class','canvas chart')
-        .attr('transform','translate('+margin.l+','+margin.t+')');
-    var tooltip = ctx.select('.custom-tooltip');
-    var visualTarget = canvas.append('rect')
-        .attr('class','target')
-        .style('fill','none')
-        .style('stroke-width','1.5px')
-        .style('stroke','#0092c8')
-        .style('opacity',0)
-
-    var rangeLabels = canvas.selectAll('.range-label')
-        .data(tracts[0].timeShares)
-        .enter()
-        .append('text')
-        .attr('class','range-label')
-        .text(function(d){return d.time})
-        .attr('transform',function(d,i){
-            var x = i*cellWidth + cellWidth/2;
-            return 'translate('+x+',0)rotate(-45)'
-        })
-        .attr('dy',-3);
-
-    //Nest tracts by neighborhoods
-    //And remove those in HI
-    //Compute neighorhood trip total
-    var hoods = d3.nest()
-        .key(function(d){return d.neighborhood})
-        .map(tracts,d3.map);
-    hoods.remove('Harbor Islands');
-
-    hoods = hoods.entries();
-    hoods
-        .sort(function(a,b){
-            if(a.key > b.key){
-                return 1
-            }else if(a.key < b.key){
-                return -1;
-            }
-        });
-    hoods.forEach(function(h){
-        h.total = 0;
-        h.value.forEach(function(tract){
-            h.total += tract.total;
-        });
-    });
-
-    //Set up the scales
-    scaleY
-        .domain([0,cityTripTotal])
-        .range([0,height-(hoods.length-1)*yPadding]);
-
-
-    //Start drawings
-    var node = canvas.selectAll('.node')
-        .data(chartLayout(hoods),function(d){return d.key});
-    var nodeEnter = node.enter()
-        .insert('g','.target')
-        .attr('class',function(d){
-            if(d.depth == 0){ return 'node share'}
-            else if(d.depth == 1){return 'node tract'}
-            else{return 'node neighborhood'}
-        })
-        .attr('transform',function(d){
-            return 'translate('+d.x+','+d.y+')';
-        });
-    nodeEnter.filter(function(d){return d.depth == 2; })
-        .append('text')
-        .text(function(d){
-            return d.key;
-        })
-        .attr('text-anchor','end')
-        .attr('dy',10)
-        .attr('dx',-5);
-    var shareNode = nodeEnter.filter(function(d){return d.depth == 0; })
-        .append('rect')
-        .on('mouseover',onShareEnter)
-        .on('mousemove',onShareHover)
-        .on('mouseout',onShareOut)
-        .attr('width',0)
-        .attr('height',function(d){return d.dy;})
-        .style('fill','white');
-    shareNode
-        .transition()
-        .duration(400)
-        .attr('width',function(d){return d.dx;})
-        .style('fill',function(d){
-            return scaleColor(d.share);
-        });
-
-    canvas.on('mousemove',function(){
-        fisheye.focus(d3.mouse(this));
-
-        node.each(function(d){
-            d.fisheye = fisheye(d);
-            })
-            .attr('transform',function(d){
-                return 'translate('+d.fisheye.x+','+d.fisheye.y+')';
-            });
-
-        shareNode
-            .attr('width',function(d){return d.dx * d.fisheye.z;})
-            .attr('height',function(d){return d.dy * d.fisheye.z;})
-    })
-
-    function onShareEnter(s){
-        dispatch.chartHover(s); //globally broadcast the id of the tract
-
-        //position visual target
-        visualTarget
-            .attr('x',s.x-5)
-            .attr('y',s.y-5)
-            .attr('width',s.dx+10)
-            .attr('height',s.dy+10)
-            .transition()
-            .style('opacity',1)
-
-        //highlight range label
-        rangeLabels
-            .style('font-weight','normal')
-            .style('fill',null)
-            .filter(function(d){
-                return d.tIndex == s.tIndex;
-            })
-            .style('font-weight','bold')
-            .style('fill','black');
-
-        tooltip.selectAll('p')
-            .remove();
-        tooltip
-            .transition()
-            .style('opacity',1);
-        tooltip
-            .append('p')
-            .html((function(){
-                if(s.subNeighborhood){
-                    return '<span class="data">'+ (s.displayName.split(','))[0] + ' </span>('+s.subNeighborhood+', '+s.neighborhood+')';
-                }else{
-                    return '<span class="data">'+ (s.displayName.split(','))[0] + ' </span>('+s.neighborhood+')';
-                }
-            })())
-        tooltip
-            .append('p')
-            .html((function(){
-                return '<span class="data">'+s.trip+'</span> workers (<span class="data">'+format(s.share)+'</span>) have commutes in the '+s.time+' minute range';
-            })())
-    }
-    function onShareHover(){
-        var xy = d3.mouse(ctx.node());
-        tooltip
-            .style('left',xy[0]-90+'px')
-            .style('bottom',(height+250-xy[1])+'px');
-    }
-    function onShareOut(){
-        dispatch.out();
-    }
-
-    dispatch.on('mapHover',function(tractId,tIndex){
-        var s;
-        node.filter(function(d){
-            return d.key == tractId+'-'+tIndex;
-            })
-            .each(function(d){
-                s = d;
-            });
-
-        visualTarget
-            .attr('x',s.x-5)
-            .attr('y',s.y-5)
-            .attr('width',s.dx+10)
-            .attr('height',s.dy+10)
-            .transition()
-            .style('opacity',1)
-
-        tooltip.selectAll('p')
-            .remove();        
-        tooltip
-            .transition()
-            .style('opacity',1);
-        tooltip
-            .style('left',s.x+s.dx/2+margin.l-90+'px')
-            .style('bottom',(height+margin.b-s.y+50)+'px');
-        tooltip
-            .append('p')
-            .html((function(){
-                if(s.subNeighborhood){
-                    return '<span class="data">'+ (s.displayName.split(','))[0] + ' </span>('+s.subNeighborhood+', '+s.neighborhood+')';
-                }else{
-                    return '<span class="data">'+ (s.displayName.split(','))[0] + ' </span>('+s.neighborhood+')';
-                }
-            })())
-        tooltip
-            .append('p')
-            .html((function(){
-                return '<span class="data">'+s.trip+'</span> workers (<span class="data">'+format(s.share)+'</span>) have commutes in the '+s.time+' minute range';
-            })());
-    })
-    dispatch.on('out.chart',function(){
-        tooltip
-            .transition()
-            .style('opacity',0);
-        visualTarget
-            .transition()
-            .style('opacity',0);
-    })
-
-    function chartLayout(hoods){
-        //a modified hierarchical layout
-        //hoods -> tracts -> shares
-        //returns a flat array of positions
-        var positions = [],
-            currentY = 0;
-
-        hoods.forEach(function(h){
-            h.x = 0;
-            h.y = currentY;
-            h.depth = 2;
-            positions.push(h);
-
-            var currentY2 = 0;
-            h.value.forEach(function(t){
-                t.x = 0;
-                t.y = currentY2 + currentY;
-                t.depth = 1;
-                t.key = t.geoid;
-                positions.push(t);
-
-                var currentX = 0;
-                t.timeShares.forEach(function(s,i){
-                    s.x = currentX;
-                    s.y = currentY2 + currentY;
-                    s.dx = cellWidth;
-                    s.dy = scaleY(t.total);
-                    s.depth = 0;
-
-                    positions.push(s);
-
-                    currentX += cellWidth;
-                })
-
-                currentY2 += scaleY(t.total);
-            })
-
-            currentY += yPadding + scaleY(h.total);
-        })
-
-        return positions;
-    }
-
-}*/
 
 function drawMap(tractGeo,hoodGeo,data,ctx){
     //Update width, height and projection, append all static elements
@@ -595,49 +378,57 @@ function drawMap(tractGeo,hoodGeo,data,ctx){
         .attr('height',30)
         .attr('x',-8)
         .attr('y',-30);
+    var myLocation = canvas.append('g')
+        .attr('class','target')
+        .style('opacity',0);
+    myLocation.append('svg:image')
+        .attr('xlink:href','assets/pin-06.svg')
+        .attr('width',16)
+        .attr('height',30)
+        .attr('x',-8)
+        .attr('y',-30);
     var metainfo = ctx.select('.canvas-meta').select('.cat');
     var legend = ctx.select('.canvas-meta')
         .append('svg')
-        .attr('width',width/2)
+        .attr('width',width)
         .attr('height',30);
     legend.selectAll('.legend-cat')
-        .data(d3.range(7))
+        .data([0,.33,.66,1,1.5,2,2.5])
         .enter()
         .append('rect')
         .attr('x',function(d,i){
-            return i*width/35
+            return i*width/20
         })
-        .attr('width',width/35)
+        .attr('width',width/20)
         .attr('height',4)
         .style('fill',function(d){
-            return scaleColor(d);
+            return scaleColorByLq(d);
         });
     legend.append('text')
-        .text('0%')
-        .attr('y',20);
-    legend.append('text')
-        .text('20%')
+        .text('Below City Avg.')
         .attr('y',20)
-        .attr('x',width/4)
-        .attr('text-anchor','end')
+    legend.append('text')
+        .text('Above City Avg.')
+        .attr('y',20)
+        .attr('x',width*6/20);
     legend
         .append('rect')
         .attr('width',width/20)
         .attr('height',4)
         .style('fill','rgb(50,50,50)')
-        .attr('x',width*6/20);
+        .attr('x',width*10/20);
     legend.append('text')
         .text('No data')
         .attr('y',20)
-        .attr('x',width*6/20);
+        .attr('x',width*10/20);
 
 
 
     //clean up data
     var _data = d3.map(data, function(d){return d.geoid2;})
 
-    //variable for displaying current time share, by default showing "carpool"
-    var mIndex = 6;
+    //variable for displaying current time share, by default showing "drive"
+    var mIndex = 1;
 
     //Draw tracts
     var tracts = canvas.selectAll('.tract')
@@ -780,6 +571,55 @@ function drawMap(tractGeo,hoodGeo,data,ctx){
             .transition()
             .style('opacity',0);
     })
+
+    //Find my address
+    $('#choropleth .address').on('change',function(e){
+        e.preventDefault();
+        var that = this;
+
+        $.ajax({
+            url:"https://maps.googleapis.com/maps/api/geocode/json",
+            data:{
+                address:$(this).val(),
+                key:apiKey,
+                bounds:"42.265742,-71.171837|42.435858,-70.953140"
+            }
+        }).done(function(data){
+            if(data.status=="OK"){
+                var result = data.results[0],
+                    latLng = result.geometry.location;
+                $(that).val(result.formatted_address);
+
+                var lngLat = [latLng.lng, latLng.lat],
+                    xy = projection(lngLat);
+
+                myLocation
+                    .transition()
+                    .style('opacity',1)
+                    .attr('transform','translate('+xy[0]+','+xy[1]+')');
+
+            }else if(data.status == "ZERO_RESULTS"){
+                $(that).val("Not found, try again?");
+
+                myLocation
+                    .transition()
+                    .style('opacity',0)
+            }else{
+                geocodeError();
+            }
+        }).fail(geocodeError)
+        .always(function(){
+        });
+
+        function geocodeError(){
+            $(that).val("An error occurred");
+
+            myLocation
+                .transition()
+                .style('opacity',0)
+        }
+    })
+
 
 }
 
